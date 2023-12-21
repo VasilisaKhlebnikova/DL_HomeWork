acdb690fee5ab75e08d405da4e99cb9fc6dee6cc
VasilisaKhlebnikova <59034827+VasilisaKhlebnikova@users.noreply.github.com>
2023-12-21 09:23:44 +0300

    Update hw_3.py

--- hw_3.py
+++ hw_3.py
@@ -22,6 +22,7 @@ test_cifar100_dataset = data_utils.Subset(test_cifar100_dataset, test_indices)
 train_cifar100_dataloader = DataLoader(dataset=train_cifar100_dataset, batch_size=1, shuffle=True)
 test_cifar100_dataloader = DataLoader(dataset=test_cifar100_dataset, batch_size=1, shuffle=True)
 
+
 class CIFAR100PredictorPerceptron(torch.nn.Module):
     def __init__(self, input_size: int, hidden_size: int, output_size: int):
         super().__init__()
@@ -41,4 +42,47 @@ class CIFAR100PredictorPerceptron(torch.nn.Module):
         x = self.softmax(x)
 
         return x
-    
+
+
+model = CIFAR100PredictorPerceptron(input_size=3*1024, hidden_size=180, output_size=100)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+num_epochs = 20
+
+for epoch in range(num_epochs):
+    train_error = 0
+    train_correct = 0
+    for x, y in train_cifar100_dataloader:
+        model.train()
+        optimizer.zero_grad()
+        prediction = model(x)
+        zero_tensor = torch.zeros_like(prediction)
+        loss = loss_fn(prediction, y)
+        train_error += loss
+        predicted_indices = torch.argmax(prediction)
+        train_correct += (predicted_indices == y).float().sum()
+
+        loss.backward()
+        optimizer.step()
+
+    writer.add_scalar('Train_loss', train_error/len(train_cifar100_dataset), epoch)
+    writer.add_scalar('Train_accuracy', train_correct/len(test_cifar100_dataset), epoch)
+
+    test_error = 0
+    test_correct = 0
+    for x, y in test_cifar100_dataloader:
+        model.eval()
+        optimizer.zero_grad()
+        prediction = model(x)
+        zero_tensor = torch.zeros_like(prediction)
+        loss = loss_fn(prediction, y)
+        test_error += loss
+        predicted_indices = torch.argmax(prediction)
+        test_correct += (predicted_indices == y).float().sum()
+
+        loss.backward()
+        optimizer.step()
+
+    writer.add_scalar('Test_loss', test_error/len(train_cifar100_dataset), epoch)
+    writer.add_scalar('Test_accuracy', test_correct/len(test_cifar100_dataset), epoch)
